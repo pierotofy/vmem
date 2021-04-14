9ee2a688c8464c507296a13bc1ea62ed8f18f131
Piero Toffanin <pt@masseranolabs.com>
2021-04-14 11:49:19 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+import sys
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+INSTALL_REQUIRES=[]
+
+setuptools.setup(
+    name="vmem",
+    version="1.0.0",
+    author="Piero Toffanin",
+    author_email="pt@uav4geo.com",
+    description="Cross-platform virtual memory information",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pierotofy/vmem",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=INSTALL_REQUIRES
+)
